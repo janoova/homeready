0d66477226f91020a2f5686b61f825d1d921d25e
Paras Bokhari <parasbokhari@gmail.com>
2026-04-08 22:49:28 -0500

    branding updates

--- components/magicui/shine-border.jsx
+++ components/magicui/shine-border.jsx
@@ -1,4 +1,4 @@
-"use client";;
+"use client";
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
@@ -17,29 +17,28 @@ export function ShineBorder({
   ...props
 }) {
   return (
-    (<div
-      style={
-        {
-          "--border-width": `${borderWidth}px`,
-          "--duration": `${duration}s`,
+    <div
+      style={{
+        "--border-width": `${borderWidth}px`,
+        "--duration": `${duration}s`,
 
-          backgroundImage: `radial-gradient(transparent,transparent, ${
-            Array.isArray(shineColor) ? shineColor.join(",") : shineColor
-          },transparent,transparent)`,
+        backgroundImage: `radial-gradient(transparent,transparent, ${
+          Array.isArray(shineColor) ? shineColor.join(",") : shineColor
+        },transparent,transparent)`,
 
-          backgroundSize: "300% 300%",
-          mask: `linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)`,
-          WebkitMask: `linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)`,
-          WebkitMaskComposite: "xor",
-          maskComposite: "exclude",
-          padding: "var(--border-width)",
-          ...style
-        }
-      }
+        backgroundSize: "300% 300%",
+        mask: `linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)`,
+        WebkitMask: `linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)`,
+        WebkitMaskComposite: "xor",
+        maskComposite: "exclude",
+        padding: "var(--border-width)",
+        ...style,
+      }}
       className={cn(
-        "pointer-events-none absolute inset-0 size-full rounded-[inherit] will-change-[background-position] motion-safe:animate-shine",
-        className
+        "border-beam pointer-events-none absolute inset-0 size-full rounded-[inherit] will-change-[background-position] motion-safe:animate-shine",
+        className,
       )}
-      {...props} />)
+      {...props}
+    />
   );
 }
